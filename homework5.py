95dd979b762a7dc89d4c48b0ba22c1100894624e
Vladimir <168404596+Daemonicola@users.noreply.github.com>
2024-05-03 00:16:35 -0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['apple', 'orange', 'kiwi', 'banana', 'cherry']
+print(my_list)
+print(my_list[0],my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'peach'
+print(my_list)
+my_dict = {'Огонь':'fire', 'синий':'blue', 'машина':'car'}
+keys_list = list(my_dict.keys())
+print(keys_list)
+my_dict.update({'книга':'book','стол':'table'})
+print(my_dict)
